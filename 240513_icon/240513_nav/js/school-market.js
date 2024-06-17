3c7f2c83d06c565e31c5c479b7bc54123cc9e87a
김예나 <w2320@e-mirim.hs.kr>
2024-06-17 14:41:19 +0900

    js search name category

--- 240513_icon/240513_nav/js/school-market.js
+++ 240513_icon/240513_nav/js/school-market.js
@@ -1,4 +1,6 @@
-const setData = (data) => {
+let allData;
+
+const showData = (data) => {
     let productContainerString = " ";
     //data 하나씩 꺼내기
     data.forEach(element => {
@@ -13,7 +15,10 @@ const setData = (data) => {
     const productContainerDiv = document.getElementsByClassName("product-container")[0];
     productContainerDiv.innerHTML = productContainerString;
 }
-
+const setData = (data) => {
+    allData = data; //처음 한 번 전체 data 보관하자
+    showData(data);
+}
 const getData = () => {
     const filename = 'js/data.json';
 
@@ -23,4 +28,13 @@ const getData = () => {
         .catch((error) => console.log(error));
 }
 
+
+
 getData(); // 호출 오타 수정 완료
+
+const searchData = (query) => {
+    if (query === "") showData(allData);        //아무것도 입력하지 않으면, 전체 data를 보여주자
+    //전체 data에서 하나 꺼내, name에 query가 있는지 확인하자. 
+    let data = allData.filter((Onedata) => Onedata["name"].includes(query) || Onedata["category"].includes(query));
+    showData(data);
+}
