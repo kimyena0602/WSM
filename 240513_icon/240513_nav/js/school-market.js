b60774ac6d171691628982d85cedc23c0e65ea29
김예나 <w2320@e-mirim.hs.kr>
2024-06-17 14:00:47 +0900

    js data.json <article>

--- 240513_icon/240513_nav/js/school-market.js
+++ 240513_icon/240513_nav/js/school-market.js
@@ -1,25 +1,26 @@
-//data.json -> js -> HTML
-
 const setData = (data) => {
-    let productContainerString = "";
-    //data 하나씩 꺼내서
-    data.froEach(element => {
-        //article 만들어
-    let articleString = `<article class="product-item">
-        <img src="images/${element.image}" alt="">
-            <div class="name">${element.name}</div>
-    </article>\n`;
-    productContainerString += articleString;
-     });
+    let productContainerString = " ";
+    //data 하나씩 꺼내기
+    data.forEach(element => {
+        //article 만들기
+        let articleString = `<article class="product-item">
+            <img src="images/${element.image}" alt="">
+                <div class="name">${element.name}</div>
+        </article>\n`;  
+        productContainerString += articleString;
+    });
     //.product-container 추가
-    const productContainerDiv = document.getElementsByClassName("product-containe")[0];
+    const productContainerDiv = document.getElementsByClassName("product-container")[0];
     productContainerDiv.innerHTML = productContainerString;
 }
+
 const getData = () => {
     const filename = 'js/data.json';
-    fetch(filename) //백엔드 있을 때 여기에 백엔드 API URL이 들어감.
+
+    fetch(filename) // 백엔드 API가 있을 때는 여기에 API 주소를 입력합니다.
         .then((response) => response.json())
         .then((data) => setData(data))
         .catch((error) => console.log(error));
 }
-getData();
+
+getData(); // 호출 오타 수정 완료
